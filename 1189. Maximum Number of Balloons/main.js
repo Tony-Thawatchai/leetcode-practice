24c5e2db45d8056896ad93c2f999603953606c5a
Tony-Thawatchai <au.thawatchai@gmail.com>
2024-10-19 01:39:12 -0700

    2024-10-19

--- 1189. Maximum Number of Balloons/main.js
+++ 1189. Maximum Number of Balloons/main.js
@@ -3,6 +3,43 @@
  * @return {number}
  */
 var maxNumberOfBalloons = function (text) {
+  //   let answerObj = {};
+  //   for (let i = 0; i < text.length; i++) {
+  //     answerObj[text[i]]
+  //       ? (answerObj[text[i]] = answerObj[text[i]] + 1)
+  //       : (answerObj[text[i]] = 1);
+  //   }
+
+  //   let counter = 0;
+
+  //   if (
+  //     !answerObj["b"] &&
+  //     !answerObj["a"] &&
+  //     !answerObj["l"] &&
+  //     !answerObj["o"] &&
+  //     !answerObj["n"]
+  //   ) {
+  //     return 0;
+  //   } else {
+  //     while (
+  //       answerObj["b"] >= 1 &&
+  //       answerObj["a"] >= 1 &&
+  //       answerObj["l"] >= 2 &&
+  //       answerObj["o"] >= 2 &&
+  //       answerObj["n"] >= 1
+  //     ) {
+  //       answerObj["b"]--;
+  //       answerObj["a"]--;
+  //       answerObj["l"] -= 2;
+  //       answerObj["o"] -= 2;
+  //       answerObj["n"]--;
+
+  //       counter++;
+  //     }
+  //   }
+
+  //   return counter;
+
   let answerObj = {};
   for (let i = 0; i < text.length; i++) {
     answerObj[text[i]]
@@ -21,22 +58,10 @@ var maxNumberOfBalloons = function (text) {
   ) {
     return 0;
   } else {
-    while (
-      answerObj["b"] >= 1 &&
-      answerObj["a"] >= 1 &&
-      answerObj["l"] >= 2 &&
-      answerObj["o"] >= 2 &&
-      answerObj["n"] >= 1
-    ) {
-      answerObj["b"]--;
-      answerObj["a"]--;
-      answerObj["l"] -= 2;
-      answerObj["o"] -= 2;
-      answerObj["n"]--;
+   
+    return Math.min(counter['b'], counter['a'], Math.floor(counter['l'] / 2), Math.floor(counter['o'] / 2), counter['n']);
 
-      counter++;
     }
   }
 
-  return counter;
 };
